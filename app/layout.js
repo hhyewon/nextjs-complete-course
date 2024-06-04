133d15b68a1052edb73276fdb4367288150cc904
hiimhyewon <HWUHLyVper5mDbGxBqXPyM3omwE1DOT+Z/UNy8izQ70=>
2024-06-04 13:06:58 +0900

    nextjs 동적 경로 및 매개변수 사용

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,16 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'NextJS Course App',
-  description: 'Your first NextJS app!',
+    title: "NextJS Course App",
+    description: "Your first NextJS app!",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    // layout: page를 감싸고 있음
+    // children: 현재 활성화 중인 page.js
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
